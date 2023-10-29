1d6d197b48bbe89ebf6cd0182522a3634f6aa90e
Ankit Dogra <ankit.dogra2002@gmail.com>
2023-10-29 22:56:02 +0530

    Update glass-type-predictor.py

--- glass-type-predictor.py
+++ glass-type-predictor.py
@@ -33,18 +33,17 @@ def load_data():
 
 glass_df = load_data() 
 
-# Creating the features data-frame holding all the columns except the last column.
+# Spliting data into features (X) and target (y)
 X = glass_df.iloc[:, :-1]
-
-# Creating the target series that holds last column.
 y = glass_df['GlassType']
 
 # Spliting the data into training and testing sets.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
 
-
+# Creating a function for glass type prediction
 @st.cache_data()
 def pred_function(mod_nam , ri , na , mg , al , si , k , ca , ba , fe):
+  # Using the provided feature values to make predictions
   glass_type = mod_nam.predict([[ri , na , mg , al , si , k , ca , ba , fe]])
   glass_type = glass_type[0]
   if glass_type == 1:
@@ -67,48 +66,72 @@ def pred_function(mod_nam , ri , na , mg , al , si , k , ca , ba , fe):
 
   else:
     return "headlamp"
-# S4.1: Add title on the main page and in the sidebar.
+  
+  
+# Adding a title to the main page and in the sidebar.
 st.title("Glass Type Predictor")
 st.sidebar.title("Exploratory Data Analysis")
-# S5.1: Using the 'if' statement, display raw data on the click of the checkbox.
+
+
+# Displaying raw data on the click of the checkbox
 if st.sidebar.checkbox("Show raw data"):
   st.subheader("Full Dataset")
   st.dataframe(glass_df)
-# S6.1: Scatter Plot between the features and the target variable.
-# Add a subheader in the sidebar with label "Scatter Plot".
+  
+  
+# Scatter Plot between the features and the target variable
 st.sidebar.subheader("Scatter Plot")
-# Choosing x-axis values for the scatter plot.
-# Add a multiselect in the sidebar with the 'Select the x-axis values:' label
-# and pass all the 9 features as a tuple i.e. ('RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe') as options.
-# Store the current value of this widget in the 'features_list' variable.
 features_list = st.sidebar.multiselect('Select the x-axis values:' , ('RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe'), key="feature_multiselect")
-# S6.2: Create scatter plots between the features and the target variable.
-# Remove deprecation warning.
 st.set_option('deprecation.showPyplotGlobalUse', False)
 for i in features_list:
   st.subheader(f"Scatter Plot between {i} and Glass Type")
   plt.figure(figsize = (15 , 10))
   sns.scatterplot(x = glass_df[i] , y = glass_df["GlassType"])
   st.pyplot()
-# S6.3: Create histograms for all the features.
-# Sidebar for histograms.
+  
+  
+# Histograms for the features
 st.sidebar.subheader("Histogram")
-# Choosing features for histograms.
 hist_features = st.sidebar.multiselect('Select the x-axis values:' , ('RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe'), key="hist_multiselect")
-# Create histograms.
 for i in hist_features:
   st.subheader(f"Histogram for {i}")
   plt.figure(figsize = (15 , 10))
   plt.hist(glass_df[i] , bins = "sturges" , edgecolor = "black")
   st.pyplot()
-# S6.4: Create box plots for all the columns.
-# Sidebar for box plots.
+
+
+# Box Plots for the columns
 st.sidebar.subheader("Boxplot")
-# Choosing columns for box plots.
 box_features = st.sidebar.multiselect('Select the x-axis values:' , ('RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe'), key="box_multiselect")
-# Create box plots.
 for i in box_features:
   st.subheader(f"Boxplot for {i}")
   plt.figure(figsize = (15 , 10))
   sns.boxplot(glass_df[i])
   st.pyplot()  
+  
+# Machine Learning Model Selection
+st.sidebar.subheader("Machine Learning Model")
+selected_model = st.sidebar.selectbox("Select a Machine Learning Model", ("Support Vector Machine (SVM)", "Random Forest", "Logistic Regression"))
+# Input Feature Values
+st.sidebar.subheader("Input Feature Values")
+ri = st.sidebar.number_input("Refractive Index (RI)", value=0.0, step=0.01)
+na = st.sidebar.number_input("Sodium (Na)", value=0.0, step=0.01)
+mg = st.sidebar.number_input("Magnesium (Mg)", value=0.0, step=0.01)
+al = st.sidebar.number_input("Aluminum (Al)", value=0.0, step=0.01)
+si = st.sidebar.number_input("Silicon (Si)", value=0.0, step=0.01)
+k = st.sidebar.number_input("Potassium (K)", value=0.0, step=0.01)
+ca = st.sidebar.number_input("Calcium (Ca)", value=0.0, step=0.01)
+ba = st.sidebar.number_input("Barium (Ba)", value=0.0, step=0.01)
+fe = st.sidebar.number_input("Iron (Fe)", value=0.0, step=0.01)
+
+# Creating an empty placeholder to display the glass type prediction
+glass_type_prediction = ""
+
+# A dictionary to map model names to their corresponding classifiers
+models = {
+    "Support Vector Machine (SVM)": SVC(),
+    "Random Forest": RandomForestClassifier(),
+    "Logistic Regression": LogisticRegression()
+}
+
+# Getting the selected classifier based on the user's choice.
